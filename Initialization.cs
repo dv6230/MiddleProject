66a97fb2220f9e4d27594ad8cdc06e2a49345d0e
dv6230 <67547654+dv6230@users.noreply.github.com>
2022-01-20 00:39:53 +0800

    m

---
+++ Initialization.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.SqlClient;
+
+namespace MiddleProject
+{
+    class Initialization
+    {
+
+        public Initialization()
+        {
+            getFood();
+            getUserAuth();
+        }
+
+        void getFood()
+        {
+            var productList = Model.GolbalVar.productList;
+            string sql = "SELECT * FROM products";
+            SqlConnection con = new SqlConnection(DBConnection.DBstr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string foodType = reader["foodType"].ToString();
+
+                var p = new Model.Product();
+                p.Name = reader["name"].ToString();
+                p.Price = (int) reader["price"];
+                p.FoodType = foodType;
+
+                productList[foodType].Add(p);
+            }
+            reader.Close();
+            con.Close();
+        }
+
+        void getUserAuth()
+        {
+            var productList = Model.GolbalVar.userPermissionList;
+            string sql = "SELECT * FROM userPermission WHERE id = @Id";
+            SqlConnection con = new SqlConnection(DBConnection.DBstr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id",Model.GolbalVar.userId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                productList.Add(reader["name"].ToString());
+            }
+            reader.Close();
+            con.Close();
+        }
+
+
+
+    }
+}
--- Panel/UserPurchaseItem.Designer.cs
+++ Panel/UserPurchaseItem.Designer.cs
@@ -31,23 +31,22 @@ namespace MiddleProject.Panel
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.lblCount = new System.Windows.Forms.Label();
-            this.lblPrice = new System.Windows.Forms.Label();
             this.lblTotalPrice = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.lblNote = new System.Windows.Forms.Label();
             this.btnDeleteItem = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(68, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 30);
             this.label1.TabIndex = 0;
@@ -57,27 +56,17 @@ namespace MiddleProject.Panel
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(121, 0);
+            this.label2.Location = new System.Drawing.Point(186, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 30);
+            this.label2.Size = new System.Drawing.Size(65, 30);
             this.label2.TabIndex = 1;
             this.label2.Text = "數量";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(187, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 30);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "單價";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(261, 0);
+            this.label4.Location = new System.Drawing.Point(326, 16);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 30);
             this.label4.TabIndex = 3;
@@ -88,7 +77,7 @@ namespace MiddleProject.Panel
             // 
             this.lblName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.lblName.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(3, 30);
+            this.lblName.Location = new System.Drawing.Point(68, 46);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(112, 30);
             this.lblName.TabIndex = 4;
@@ -98,27 +87,17 @@ namespace MiddleProject.Panel
             // 
             this.lblCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.lblCount.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCount.Location = new System.Drawing.Point(121, 30);
+            this.lblCount.Location = new System.Drawing.Point(186, 46);
             this.lblCount.Name = "lblCount";
             this.lblCount.Size = new System.Drawing.Size(60, 30);
             this.lblCount.TabIndex = 5;
             this.lblCount.Text = "OOXX茶";
             // 
-            // lblPrice
-            // 
-            this.lblPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.lblPrice.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrice.Location = new System.Drawing.Point(187, 30);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(68, 30);
-            this.lblPrice.TabIndex = 6;
-            this.lblPrice.Text = "OOXX茶";
-            // 
             // lblTotalPrice
             // 
             this.lblTotalPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.lblTotalPrice.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalPrice.Location = new System.Drawing.Point(261, 30);
+            this.lblTotalPrice.Location = new System.Drawing.Point(326, 46);
             this.lblTotalPrice.Name = "lblTotalPrice";
             this.lblTotalPrice.Size = new System.Drawing.Size(79, 30);
             this.lblTotalPrice.TabIndex = 7;
@@ -127,7 +106,7 @@ namespace MiddleProject.Panel
             // label9
             // 
             this.label9.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(11, 100);
+            this.label9.Location = new System.Drawing.Point(15, 80);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(76, 38);
             this.label9.TabIndex = 8;
@@ -138,55 +117,77 @@ namespace MiddleProject.Panel
             // 
             this.lblNote.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.lblNote.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNote.Location = new System.Drawing.Point(87, 100);
+            this.lblNote.Location = new System.Drawing.Point(18, 125);
             this.lblNote.Name = "lblNote";
-            this.lblNote.Size = new System.Drawing.Size(370, 38);
+            this.lblNote.Size = new System.Drawing.Size(387, 38);
             this.lblNote.TabIndex = 9;
             this.lblNote.Text = "備註";
             this.lblNote.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // btnDeleteItem
             // 
-            this.btnDeleteItem.Location = new System.Drawing.Point(373, 26);
+            this.btnDeleteItem.Location = new System.Drawing.Point(321, 86);
             this.btnDeleteItem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDeleteItem.Name = "btnDeleteItem";
-            this.btnDeleteItem.Size = new System.Drawing.Size(84, 54);
+            this.btnDeleteItem.Size = new System.Drawing.Size(84, 35);
             this.btnDeleteItem.TabIndex = 10;
             this.btnDeleteItem.Text = "刪除";
             this.btnDeleteItem.UseVisualStyleBackColor = true;
             this.btnDeleteItem.Click += new System.EventHandler(this.btnDeleteItem_Click);
             // 
-            // flowLayoutPanel1
+            // label3
             // 
-            this.flowLayoutPanel1.Controls.Add(this.label1);
-            this.flowLayoutPanel1.Controls.Add(this.label2);
-            this.flowLayoutPanel1.Controls.Add(this.label3);
-            this.flowLayoutPanel1.Controls.Add(this.label4);
-            this.flowLayoutPanel1.Controls.Add(this.lblName);
-            this.flowLayoutPanel1.Controls.Add(this.lblCount);
-            this.flowLayoutPanel1.Controls.Add(this.lblPrice);
-            this.flowLayoutPanel1.Controls.Add(this.lblTotalPrice);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 20);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(350, 77);
-            this.flowLayoutPanel1.TabIndex = 11;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(252, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "單價";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblPrice.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(252, 46);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(68, 30);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "OOXX茶";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("新細明體", 12F);
+            this.numberLabel.Location = new System.Drawing.Point(16, 22);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(32, 24);
+            this.numberLabel.TabIndex = 11;
+            this.numberLabel.Text = "99";
             // 
             // UserPurchaseItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnDeleteItem);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblPrice);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "UserPurchaseItem";
-            this.Size = new System.Drawing.Size(468, 157);
+            this.Size = new System.Drawing.Size(419, 180);
             this.Load += new System.EventHandler(this.UserPurchaseItem_Load);
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -194,15 +195,15 @@ namespace MiddleProject.Panel
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label9;
         public System.Windows.Forms.Label lblName;
         public System.Windows.Forms.Label lblCount;
-        public System.Windows.Forms.Label lblPrice;
         public System.Windows.Forms.Label lblTotalPrice;
         public System.Windows.Forms.Label lblNote;
         public System.Windows.Forms.Button btnDeleteItem;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label lblPrice;
+        public System.Windows.Forms.Label numberLabel;
     }
 }
