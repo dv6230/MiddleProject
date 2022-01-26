31291197ff669e3b8e5af9c371734dc6ced4f389
dveasia558 <dveasia558@gmail.com>
2022-01-26 16:31:34 +0800

    s

--- DataBaseStruct/SQLQuery.sql
+++ DataBaseStruct/SQLQuery.sql
@@ -1,6 +1,6 @@
 USE [MidProject]
 GO
-/****** Object:  Table [dbo].[accounts]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[accounts]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -20,25 +20,28 @@ CREATE TABLE [dbo].[accounts](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[custumer]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[customer]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[custumer](
+CREATE TABLE [dbo].[customer](
 	[Id] [int] IDENTITY(1000,1) NOT NULL,
-	[name] [nvarchar](100) NULL,
-	[phone] [nvarchar](12) NULL,
+	[name] [nvarchar](100) NOT NULL,
+	[phone] [nvarchar](10) NOT NULL,
 	[email] [nvarchar](100) NULL,
 	[address] [nvarchar](250) NULL,
 	[point] [int] NOT NULL,
+	[gender] [int] NULL,
+	[createDate] [datetime] NOT NULL,
+	[birth] [date] NULL,
  CONSTRAINT [PK_custumer] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ingredients]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[ingredients]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -47,45 +50,32 @@ CREATE TABLE [dbo].[ingredients](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [nvarchar](50) NOT NULL,
 	[price] [int] NOT NULL,
- CONSTRAINT [PK_ingredients] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ingredients_1] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[orderDetail]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[orderDetail]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[orderDetail](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [nchar](10) NULL,
+	[orderId] [int] NOT NULL,
+	[name] [nvarchar](50) NULL,
 	[count] [int] NULL,
 	[price] [int] NULL,
 	[note] [nvarchar](200) NULL,
+	[ingredient] [nvarchar](200) NULL,
  CONSTRAINT [PK_orderDetail] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[orderIngredient]    Script Date: 2022/1/20 上午 09:12:24 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[orderIngredient](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[orderDetailId] [int] NOT NULL,
-	[ingredientId] [int] NOT NULL,
- CONSTRAINT [PK_orderIngredient] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[orders]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[orders]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -102,7 +92,37 @@ CREATE TABLE [dbo].[orders](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[products]    Script Date: 2022/1/20 上午 09:12:24 ******/
+/****** Object:  Table [dbo].[permission]    Script Date: 2022/1/26 下午 04:31:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[permission](
+	[Id] [int] NOT NULL,
+	[accountId] [int] NOT NULL,
+	[permitName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_permission] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[productIngredient]    Script Date: 2022/1/26 下午 04:31:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[productIngredient](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[productId] [int] NOT NULL,
+	[ingredientId] [int] NOT NULL,
+ CONSTRAINT [PK_productIngredient] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[products]    Script Date: 2022/1/26 下午 04:31:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -112,16 +132,42 @@ CREATE TABLE [dbo].[products](
 	[name] [nvarchar](50) NOT NULL,
 	[price] [int] NOT NULL,
 	[note] [nvarchar](100) NULL,
-	[productTypeId] [int] NULL,
+	[productTypeId] [int] NOT NULL,
  CONSTRAINT [PK_products] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[custumer] ADD  CONSTRAINT [DF_custumer_point]  DEFAULT ((0)) FOR [point]
+/****** Object:  Table [dbo].[productType]    Script Date: 2022/1/26 下午 04:31:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[productType](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_productType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[customer] ADD  CONSTRAINT [DF_custumer_point]  DEFAULT ((0)) FOR [point]
 GO
 ALTER TABLE [dbo].[ingredients] ADD  CONSTRAINT [DF_ingredients_price]  DEFAULT ((0)) FOR [price]
 GO
 ALTER TABLE [dbo].[orders] ADD  CONSTRAINT [DF_orders_orderDateTime]  DEFAULT (getdate()) FOR [orderDateTime]
+GO
+ALTER TABLE [dbo].[products] ADD  CONSTRAINT [DF_products_productTypeId]  DEFAULT ((1)) FOR [productTypeId]
+GO
+ALTER TABLE [dbo].[products]  WITH CHECK ADD  CONSTRAINT [FK_products_productType] FOREIGN KEY([productTypeId])
+REFERENCES [dbo].[productType] ([Id])
+GO
+ALTER TABLE [dbo].[products] CHECK CONSTRAINT [FK_products_productType]
+GO
+ALTER TABLE [dbo].[productType]  WITH CHECK ADD  CONSTRAINT [FK_productType_productType] FOREIGN KEY([Id])
+REFERENCES [dbo].[productType] ([Id])
+GO
+ALTER TABLE [dbo].[productType] CHECK CONSTRAINT [FK_productType_productType]
 GO
